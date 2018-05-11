6dd9336335d646ca2b147ac3ff4f409badd73426
Marc Seiler <mseiler@gmail.com>
2018-05-11 16:01:09 -0400

    removed initialize method

--- lib/active_record/connection_adapters/auroramysql_adapter/core.rb
+++ lib/active_record/connection_adapters/auroramysql_adapter/core.rb
@@ -2,13 +2,8 @@ require 'active_record/connection_adapters/mysql2_adapter'
 
 module ActiveRecord
   module ConnectionAdapters
-
     class AuroraMySqlAdapter < Mysql2Adapter
       ADAPTER_NAME = "AuroraMySql".freeze
-      def initialize(connection, logger, connection_options, config)
-        super
-      end
     end
-
   end
 end
